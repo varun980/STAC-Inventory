61f9cceba806b5be78b5f8c0647fe9897b7688d2
varun980 <56244632+varun980@users.noreply.github.com>
2019-10-07 10:28:27 +0530

    Remove old secret key.

--- stacinv/settings.py
+++ stacinv/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '%d@k7irdptc)vgz=-sk5e==5&%op()eqwrxjp&#dl)g1ybn4d('
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', '%d@k7irdptc)vgz=-sk5e==5&%op()eqwrxjp&#dl)g1ybn4d(')
 
